0d400a4d5584afa9d53e5a2d7a50a6ed54a1d8b4
boady <46287830+boady@users.noreply.github.com>
2019-06-10 14:49:03 -0400

    Update Playlist.js

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -6,7 +6,7 @@ class Playlist extends React.Component {
   render() {
     return (
       <div className="Playlist">
-      <input value="New Playlist"/>
+      <input value= defaultValue = {'New Playlist'}/>
       <button className="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
     )
